970761dbb0c02aa5b84119b0635c46438d8ee8b8
Àlex <alex@mobilejazz.com>
2020-03-03 18:50:39 +0100

    docker env and test files

---
+++ sample/bin/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Stop script on error
+set -euo pipefail
+
+HELPERS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/helpers" >/dev/null 2>&1 && pwd)"
+source "${HELPERS_DIR}"/functions.sh
+
+logo
+
+# Host User and Group Id to avoid permissions problems
+DOCKER_PHP_USER_ID=$(id -u)
+DOCKER_PHP_GROUP_ID=$(id -g)
+
+# Re-Build Dockerfile
+docker_compose down
+docker_compose build \
+    --compress \
+    --force-rm \
+    --memory 1GB \
+    --build-arg HOST_USER_ID="${DOCKER_PHP_USER_ID}" \
+    --build-arg HOST_GROUP_ID="${DOCKER_PHP_GROUP_ID}"
---
+++ sample/src/public/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "Before start break of XDebug";
+
+xdebug_break();
+var_dump($_SERVER);
+
+echo "After start break of XDebug";
