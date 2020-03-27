2b1ee8f383ae509ba673182d0b0875ea595afafb
Àlex <alex@mobilejazz.com>
2020-03-27 18:02:53 +0100

    new blankmapper

---
+++ core/src/repository/mapper/BlankMapper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace harmony\core\repository\mapper;
+
+class BlankMapper implements Mapper
+{
+    /**
+     * @inheritDoc
+     */
+    public function map($from)
+    {
+        return $from;
+    }
+}
