8fab13b079665bdb50b533ff7d789c8f761894bc
Alex-DA <Alex-DA@users.noreply.github.com>
2021-07-15 11:27:04 +0200

    fix linter

--- core/src/Module/Symfony/DependencyInjection/SymfonyModuleGetService.php
+++ core/src/Module/Symfony/DependencyInjection/SymfonyModuleGetService.php
@@ -3,13 +3,13 @@
 namespace Harmony\Core\Module\Symfony\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
-// @phpstan-ignore-next-line
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
-/**
- * @psalm-suppress UndefinedFunction
- */
 trait SymfonyModuleGetService {
+  /**
+   * @psalm-suppress UndefinedFunction
+   * @phpstan-ignore-next-line
+   */
   protected function getService(string $serviceName): ReferenceConfigurator {
     return service($serviceName);
   }
