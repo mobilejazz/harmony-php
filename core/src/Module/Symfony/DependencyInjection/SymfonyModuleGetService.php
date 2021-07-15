072789c8b555f7d480ac0c419cc7250eaee3c933
Alex-DA <Alex-DA@users.noreply.github.com>
2021-07-15 11:48:39 +0200

    fix phpstan lint

--- core/src/Module/Symfony/DependencyInjection/SymfonyModuleGetService.php
+++ core/src/Module/Symfony/DependencyInjection/SymfonyModuleGetService.php
@@ -3,12 +3,13 @@
 namespace Harmony\Core\Module\Symfony\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
+
+// @phpstan-ignore-next-line
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 trait SymfonyModuleGetService {
   /**
    * @psalm-suppress UndefinedFunction
-   * @phpstan-ignore-next-line
    */
   protected function getService(string $serviceName): ReferenceConfigurator {
     return service($serviceName);
