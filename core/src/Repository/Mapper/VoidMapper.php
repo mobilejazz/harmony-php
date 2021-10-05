f6d77b614d5f5abe39b765f48d1ae993d6645025
Oriol Janés Pereira <orioljanes@gmail.com>
2021-10-05 21:37:34 +0200

    Void Mapper

---
+++ core/src/Repository/Mapper/VoidMapper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Harmony\Core\Repository\Mapper;
+
+use Harmony\Core\Shared\Error\MethodNotImplementedException;
+
+/**
+ * @template TFrom
+ * @template TTo
+ * @implements Mapper<TFrom, TTo>
+ */
+Class VoidMapper implements Mapper {
+    /**
+     * @inheritdoc
+     */
+    public function map(mixed $from): void {
+        throw new MethodNotImplementedException();
+    }
+}
