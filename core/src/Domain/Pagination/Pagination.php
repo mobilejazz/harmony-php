34f9e0fce103c4f9e155bccaa93e9b6ab9315cbc
Oriol Janés Pereira <orioljanes@gmail.com>
2021-10-27 16:51:50 +0200

    Serializable Pagination

--- core/src/Domain/Pagination/Pagination.php
+++ core/src/Domain/Pagination/Pagination.php
@@ -6,18 +6,9 @@ namespace Harmony\Core\Domain\Pagination;
  * @template T
  */
 class Pagination {
-    /**
-     * @param array<T> $values
-     */
-    public function __construct(
-        protected array $values,
-    ) {
-    }
-
-    /**
-     * @return array<T>
-     */
-    public function getValues(): array {
-        return $this->values;
-    }
+  /**
+   * @param array<T> $values
+   */
+  public function __construct(public array $values) {
+  }
 }
--- core/src/Domain/Pagination/PaginationOffsetLimit.php
+++ core/src/Domain/Pagination/PaginationOffsetLimit.php
@@ -7,33 +7,18 @@ namespace Harmony\Core\Domain\Pagination;
  * @extends Pagination<T>
  */
 class PaginationOffsetLimit extends Pagination {
-
-    /**
-     * @param array<T> $values
-     * @param int $offset
-     * @param int $limit
-     * @param int $size
-     */
-    public function __construct(
-        protected array $values,
-        protected int $offset,
-        protected int $limit,
-        protected int $size,
-    ) {
-        parent::__construct($values);
-    }
-
-    public function getOffset(): int {
-        return $this->offset;
-    }
-
-    public function getLimit(): int
-    {
-        return $this->limit;
-    }
-
-    public function getSize(): int
-    {
-        return $this->size;
-    }
+  /**
+   * @param array<T> $values
+   * @param int $offset
+   * @param int $limit
+   * @param int $size
+   */
+  public function __construct(
+    array $values,
+    public int $offset,
+    public int $limit,
+    public int $size
+  ) {
+    parent::__construct($values);
+  }
 }
