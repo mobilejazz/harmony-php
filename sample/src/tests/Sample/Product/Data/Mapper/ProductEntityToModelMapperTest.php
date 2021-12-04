b7fa45035bc21086e2a0f6b8610d67eabfc2795e
Alex-DA <Alex-DA@users.noreply.github.com>
2021-12-04 20:14:53 +0100

    test notes

--- sample/src/tests/Sample/Product/Data/Mapper/ProductEntityToModelMapperTest.php
+++ sample/src/tests/Sample/Product/Data/Mapper/ProductEntityToModelMapperTest.php
@@ -5,15 +5,21 @@ namespace App\Tests\Sample\Product\Data\Mapper;
 use PHPUnit\Framework\TestCase;
 use Sample\Product\Data\Entity\ProductEntity;
 use Sample\Product\Data\Mapper\ProductEntityToProductMapper;
+use Sample\Product\Domain\Model\Product;
 
 class ProductEntityToModelMapperTest extends TestCase {
   public function testMap(): void {
     $entity = new ProductEntity(1234, "superName", "someDescription", 66.333);
-    $mapperModel = (new ProductEntityToProductMapper())->map($entity);
+    $model = (new ProductEntityToProductMapper())->map($entity);
 
-    $this->assertSame($entity->id, $mapperModel->id);
-    $this->assertSame($entity->name, $mapperModel->name);
-    $this->assertSame($entity->description, $mapperModel->description);
-    $this->assertSame($entity->price, $mapperModel->price);
+    $this->assertInstanceOf(Product::class, $model);
+
+    // @todo Generic Test for mappers.
+    // Reflection to compare attributes?
+    // Input From Object and check expected output
+    $this->assertSame($entity->id, $model->id);
+    $this->assertSame($entity->name, $model->name);
+    $this->assertSame($entity->description, $model->description);
+    $this->assertSame($entity->price, $model->price);
   }
 }
